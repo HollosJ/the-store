cc0c32f5f7f79d4808e841ce1604b11b8a09743e
HollosJ <hollosj99@gmail.com>
2021-10-10 00:24:25 +0100

    ✨Added filter functionality

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ItemCard from "./ItemCard"
 
 const Store = (props) => {
+    const categories = ["electronics","jewelery","men's clothing","women's clothing"]
+    const [cat, setCat] = useState("all")
+
+    //get items based on category state
+    const getItemsInCategory = () => {
+        return cat === "all" 
+        ? props.items
+        : props.items.filter(item => item.category === cat)
+    }
+
     return (
-        <div className="grid">
-           {props.items.length > 0 ? (
-               props.items.map((item) => {
+        <>
+            <select name="" id="" className="filters" onChange={(e) => setCat(e.target.value)}>
+                <option value="all">ALL</option>
+                {categories.map((item, key) => 
+                    {
+                        return <option key={key} value={item}>{item.toUpperCase()}</option>
+                    })
+                }
+            </select>
+            <div className="grid">
+            {getItemsInCategory().length > 0 
+            ? (getItemsInCategory().map((item) => {
                 return <ItemCard
                 key={item.id}
                 id={item.id}
@@ -17,8 +36,10 @@ const Store = (props) => {
                 removeFromWishlist={props.removeFromWishlist}
                 inWishlist={item.inWishlist ? true : false}/>
             })
-           ):<div className="spinner"></div>}
-        </div>
+            )
+            :<div className="spinner"></div>}
+            </div>
+        </>
     )
 }
 
